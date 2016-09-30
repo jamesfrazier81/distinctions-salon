cc906fc544b64080798b1a1c48f1f88c56932864
James Frazier <jamesfrazier81@gmail.com>
2016-09-29 20:07:52 -0700

    New functions, logo.

--- wp-content/themes/distinctions-salon/functions.php
+++ wp-content/themes/distinctions-salon/functions.php
@@ -1,13 +1,15 @@
 <?php
-
-// Enqueue styles and scripts
-add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles');
-function enqueue_child_theme_styles() {
-    wp_enqueue_style( 'grid', get_stylesheet_directory_uri() . '/bower_components/font-awesome/css/font-awesome.min.css');
+function wpdocs_theme_name_scripts() {
+    // wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+    wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/js/dist/scripts.min.js', array(), '1.0.0', true );
 }
+add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 
-function wp_enqueue_scripts() {
-    wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/js/dist/scripts.min.js', array( 'jquery' ), '1.0', true );
+// custom admin login logo
+function custom_login_logo() {
+	echo '<style type="text/css">
+	h1 a { background-image: url(/wp-content/uploads/2016/09/logo-distinctions-salon.jpg) !important; background-size: 100% !important; width: 320px !important; height: 178px !important; }
+	</style>';
 }
-
+add_action('login_head', 'custom_login_logo');
 ?>
