281c594122859e56579160390443cddcb9eefc00
James Frazier <jamesfrazier81@gmail.com>
2016-10-01 23:25:04 -0700

    Duplicate Post install. Updated nav styles. Updated content. Updated login logo link and title.

--- wp-content/themes/distinctions-salon/functions.php
+++ wp-content/themes/distinctions-salon/functions.php
@@ -1,9 +1,10 @@
 <?php
-function wpdocs_theme_name_scripts() {
-    // wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/bower_components/font-awesome/css/font-awesome.min.css' );
     wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/js/dist/scripts.min.js', array(), '1.0.0', true );
+
 }
-add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 
 //Menu registration
 function my_wp_nav_menu_args( $args = '' ) {
@@ -18,10 +19,18 @@ if( is_user_logged_in() ) {
 add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
 
 // Custom admin login logo
+add_action('login_head', 'custom_login_logo');
 function custom_login_logo() {
 	echo '<style type="text/css">
 	h1 a { background-image: url(/wp-content/uploads/2016/10/ds-logo-lg.png) !important; background-size: 100% !important; width: 320px !important; height: 93px !important; }
 	</style>';
 }
-add_action('login_head', 'custom_login_logo');
+
+// changing the logo link from wordpress.org to your site
+function mb_login_url() {  return home_url(); }
+add_filter( 'login_headerurl', 'mb_login_url' );
+
+// changing the alt text on the logo to show your site name
+function mb_login_title() { return get_option( 'blogname' ); }
+add_filter( 'login_headertitle', 'mb_login_title' );
 ?>
