0ce89f57754e869bbde7c8ac107699f9a51c46ef
James Frazier <jamesfrazier81@gmail.com>
2016-10-11 22:23:12 -0700

    New images and updated styles.
    Added Advanced Layer Slider and more.

--- wp-content/themes/distinctions-salon/js/scripts.js
+++ wp-content/themes/distinctions-salon/js/scripts.js
@@ -1,3 +1,5 @@
 jQuery(function($) {
-	$('#top.logged-in #header_meta').html('<div class="container"><div class="phone-info "><span><a href="/wp-admin/profile.php">My Account</a><a href="/wp-login.php?action=logout">Log Out</a></span></div></div>');
+
+	// Adds a new user meta menu to log into admin or log out
+	$('#top.logged-in #header_meta').html('<div class="container"><div class="phone-info"><span><a href="/wp-admin/profile.php">My Account</a><a href="/wp-login.php?action=logout">Log Out</a></span></div></div>');
 });
