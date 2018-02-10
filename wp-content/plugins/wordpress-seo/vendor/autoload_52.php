d7a90d20062855dff73b6c2394c35b605323be45
James Frazier <jamesfrazier81@gmail.com>
2018-02-10 12:24:20 -0700

    Updated WP to 4.9.4. Updated plugins. Updated Twenty Sixteen theme.

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit49e1df60badc61a0aaf04789d508de9b::getLoader();
+return ComposerAutoloaderInit104ee497197b3cf4676ee24c0ff0c7f1::getLoader();
